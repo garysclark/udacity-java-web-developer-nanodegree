d47137418e4d4558961810c789273cc14f3a7448
gary <gary@lxGaryDev>
2021-10-12 11:51:57 -0700

    Added test to navigate to the notes tab

--- cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomePage.java
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomePage.java
@@ -1,5 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,12 +11,29 @@ public class HomePage {
 	@FindBy(className = "display-5")
 	private WebElement pageTitle;
 
+	@FindBy(id = "add-note-btn")
+	private WebElement addNoteButton;
+
+	@FindBy(id = "nav-notes-tab")
+	private WebElement notesTab;
+
+	private JavascriptExecutor jse;
+
 	public HomePage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
+		jse = (JavascriptExecutor)driver;
 	}
 
 	public boolean isPageReady() {
 		return pageTitle.getAttribute("innerHTML").equals("Cloud Drive");
+	}
+
+	public void selectNotesTab() {
+		jse.executeScript("arguments[0].click()", notesTab);
+	}
+
+	public boolean isNotesTabVisible() {
+		return addNoteButton.isDisplayed();
 	}
 
 }
---
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomePageTests.java
@@ -0,0 +1,73 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.model.UserTests;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase
+public class HomePageTests {
+
+	@LocalServerPort
+	private Integer port;
+	
+	@Autowired
+	private UserService userService;
+
+	private HomePage homePage;
+
+	private LoginPage loginPage;
+	
+	private static WebDriver driver;
+	
+	@BeforeAll
+	static public void beforeAll() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@AfterAll
+	static public void afterAll() {
+		driver.quit();
+	}
+	
+	@BeforeEach
+	public void beforeEach() {
+		driver.get("http://localhost:" + port + "/home");
+		loginPage = new LoginPage(driver);
+		homePage = new HomePage(driver);
+		User user = UserTests.getTestUser_1();
+		userService.createUser(user);
+		loginPage.login(user.getUsername(),user.getPassword());
+	}
+	
+	@Test
+	public void canAccessHomePage() {
+		assertTrue(homePage.isPageReady());
+
+	}
+	
+	@Test
+	public void canSelectNotesTab() {
+		homePage.selectNotesTab();
+		assertTrue(homePage.isNotesTabVisible());
+	}
+
+}
