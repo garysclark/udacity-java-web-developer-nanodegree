2088d057fea29e4905d5d1ec8b0217f1bb78bf38
gary <gary@lxGaryDev>
2022-01-22 11:31:32 -0800

    Added MySql support

---
+++ garydata/src/main/java/com/byrneclark/garydata/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package com.byrneclark.garydata;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class DataSourceConfig {
+
+//	@Bean
+//	@ConfigurationProperties("spring.datasource")
+	public DataSource getDataSource(DataSourceProperties properties) {
+		DataSourceBuilder<?> dsb = DataSourceBuilder.create();
+		dsb.username("sa");
+		dsb.password("sysadmin*1234M");
+		dsb.url("jdbc.mysql://localhost:3306/plant");
+		DataSource dataSource = dsb.build();
+		return dataSource;
+	}
+}
