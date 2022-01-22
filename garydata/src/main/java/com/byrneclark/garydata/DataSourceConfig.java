40a3bdbba0e4a9e44caa9aa3865de073631a8109
gary <gary@lxGaryDev>
2022-01-22 14:30:18 -0800

    Problem with @ConfigurationProperties

--- garydata/src/main/java/com/byrneclark/garydata/DataSourceConfig.java
+++ garydata/src/main/java/com/byrneclark/garydata/DataSourceConfig.java
@@ -7,17 +7,19 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DataSourceConfig {
 
 	@Bean
-	@ConfigurationProperties("spring.datasource")
+	@Primary
+	@ConfigurationProperties(prefix="gary.datasource")
 	public DataSource getDataSource(DataSourceProperties properties) {
 		DataSourceBuilder<?> dsb = DataSourceBuilder.create();
 		dsb.username("sa");
 		dsb.password("sysadmin*1234M");
-		dsb.url("jdbc.mysql://localhost:3306/plant");
+//		dsb.url("jdbc:mysql://localhost:3306/plant");
 		DataSource dataSource = dsb.build();
 		return dataSource;
 	}
