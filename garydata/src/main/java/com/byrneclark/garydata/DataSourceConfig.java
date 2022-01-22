b2aaa41e43c9a9e1aafb7496ddb52c4c4410b4aa
gary <gary@lxGaryDev>
2022-01-22 14:01:06 -0800

    Fixed MySql integration issues

--- garydata/src/main/java/com/byrneclark/garydata/DataSourceConfig.java
+++ garydata/src/main/java/com/byrneclark/garydata/DataSourceConfig.java
@@ -3,10 +3,7 @@ package com.byrneclark.garydata;
 import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 public class DataSourceConfig {
