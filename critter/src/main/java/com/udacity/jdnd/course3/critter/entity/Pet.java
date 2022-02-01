229532a12d87543d0ecf252ba754d62925673668
gary <gary@lxGaryDev>
2022-01-31 20:49:40 -0800

    Completed Pet Entity

--- critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
@@ -1,11 +1,16 @@
 package com.udacity.jdnd.course3.critter.entity;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import com.udacity.jdnd.course3.critter.pet.PetType;
 
 @Entity
 public class Pet {
@@ -16,4 +21,94 @@ public class Pet {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Customer customer;
+
+	private PetType type;
+
+	private String name;
+
+	private Long ownerId;
+
+	private LocalDate birthDate;
+
+	private String notes;
+
+	public Pet(Long id, PetType type, String name, Long ownerId, LocalDate birthDate,
+			String notes) {
+		this.id = id;
+		this.type = type;
+		this.name = name;
+		this.ownerId = ownerId;
+		this.birthDate = birthDate;
+		this.notes = notes;
+	}
+
+	public Pet() {
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setType(PetType type) {
+		this.type = type;
+	}
+
+	public PetType getType() {
+		return type;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public Long getOwnerId() {
+		return ownerId;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(birthDate, customer, id, name, notes, ownerId, type);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pet other = (Pet) obj;
+		return Objects.equals(birthDate, other.birthDate) && Objects.equals(customer, other.customer)
+				&& Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(notes, other.notes) && Objects.equals(ownerId, other.ownerId) && type == other.type;
+	}
 }
---
+++ critter/src/test/java/com/udacity/jdnd/course3/critter/entity/PetTests.java
@@ -0,0 +1,65 @@
+package com.udacity.jdnd.course3.critter.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import com.udacity.jdnd.course3.critter.pet.PetType;
+
+public class PetTests {
+
+	private static final Long TEST_ID = 99l;
+	private static final PetType TEST_TYPE = PetType.DOG;
+	private static final String TEST_NAME = "Poppy";
+	private static final Long TEST_OWNER_ID = 101l;
+	private static final LocalDate TEST_BIRTHDATE = LocalDate.of(2016, 10, 21);
+	private static final String TEST_NOTES = "test notes";
+
+	@Test
+	public void canCreate() {
+		Pet pet = new Pet();
+		assertNotNull(pet);
+	}
+
+	@Test
+	public void canSetGetAttributes() {
+		Pet pet = new Pet();
+		pet.setId(TEST_ID);
+		pet.setType(TEST_TYPE);
+		pet.setName(TEST_NAME);
+		pet.setOwnerId(TEST_OWNER_ID);
+		pet.setBirthDate(TEST_BIRTHDATE);
+		pet.setNotes(TEST_NOTES);
+		validateAttributes(pet);
+	}
+
+	@Test
+	public void canCreateWithAttributes() {
+		Pet pet = getTestPet();
+		validateAttributes(pet);
+	}
+
+	@Test
+	public void canValidateEquals() {
+		Pet pet1 = getTestPet();
+		Pet pet2 = getTestPet();
+		assertEquals(pet1, pet2);
+	}
+
+	private Pet getTestPet() {
+		return new Pet(TEST_ID, TEST_TYPE, TEST_NAME, TEST_OWNER_ID, TEST_BIRTHDATE, TEST_NOTES);
+	}
+
+	private void validateAttributes(Pet pet) {
+		assertEquals(TEST_ID, pet.getId());
+		assertEquals(TEST_TYPE, pet.getType());
+		assertEquals(TEST_NAME, pet.getName());
+		assertEquals(TEST_OWNER_ID, pet.getOwnerId());
+		assertEquals(TEST_BIRTHDATE, pet.getBirthDate());
+		assertEquals(TEST_NOTES, pet.getNotes());
+	}
+
+}
