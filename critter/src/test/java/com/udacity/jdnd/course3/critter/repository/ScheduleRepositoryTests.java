9dc37d5a1d905cde3e9433e8f84b7cd0f4a82b79
gary <gary@lxGaryDev>
2022-02-05 06:15:08 -0800

    ...

--- critter/src/test/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepositoryTests.java
+++ critter/src/test/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepositoryTests.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.udacity.jdnd.course3.critter.entity.Employee;
